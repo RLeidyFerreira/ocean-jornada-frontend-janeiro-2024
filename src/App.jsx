aebe18612c202360c77b4330a111afd397a3d98f
Rosileide Ferreira <116040542+RLeidyFerreira@users.noreply.github.com>
2024-01-25 14:06:45 +0000

    Correção no codigo

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import './App.css'
 function App() {
   return (
     <>
-      <div>
-        <h2>Rick Sanches</h2>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
+      <div className="card">
+        <h2>Rick Sanchez</h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
       </div>
     </>
   )
 }
+
+export default App
